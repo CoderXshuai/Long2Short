a54f5d6427c4c664641b4aefe9398a04c6b8b70e
CoderXshuai <xshuai1024@foxmail.com>
2024-07-03 00:43:26 +0800

    feature: 查询用户无脱敏信息

---
+++ admin/src/main/java/org/example/Long2Short/admin/dto/resp/UserActualRespDTO.java
@@ -0,0 +1,42 @@
+package org.example.Long2Short.admin.dto.resp;
+
+import lombok.Data;
+
+/**
+ * @ClassName UserRespDTO
+ * @Description 用户返回参数响应
+ * @Author CoderXshuai
+ * @CreateTime 2024/6/30 1:56
+ * @Version v1.0
+ */
+
+@Data
+public class UserActualRespDTO {
+
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+}
