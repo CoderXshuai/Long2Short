110c6f75ba483b5cf37a54715f17fd1720a222cf
CoderXshuai <xshuai1024@foxmail.com>
2024-07-07 02:14:41 +0800

    feature: 开发Long2Short后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/org/example/Long2Short/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,31 @@
+package org.example.Long2Short.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @ClassName MyMetaObjectHandler
+ * @Description 自动更新字段（如创建时间、更新时间，删除标志）
+ * @Author CoderXshuai
+ * @CreateTime 2024/7/7 1:29
+ * @Version v1.0
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
